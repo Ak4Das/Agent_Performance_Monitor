21c03f8fcbb39c3b9375910aed1668cbe53f3f5c
Akash Das <akashdas02052@gmail.com>
2026-04-26 09:32:36 +0530

    Feat: Add filter functionality to Leads page

--- src/pages/Leads.jsx
+++ src/pages/Leads.jsx
@@ -2,9 +2,9 @@ import styles from "../style_modules/page_modules/Team.module.css"
 import tableStyles from "../style_modules/component_modules/Table.module.css"
 import SideBar from "../components/SideBar.jsx"
 import NavBar from "../components/NavBar.jsx"
-import { useState } from "react"
-import leadsData from "../leadData.js"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 export default function Leads() {
   const [idBtnClicked, setIdBtnClick] = useState(false)
@@ -16,7 +16,122 @@ export default function Leads() {
   const [priorityBtnClicked, setPriorityBtnClick] = useState(false)
   const [timeToCloseBtnClicked, setTimeToCloseBtnClick] = useState(false)
   const [closedAtBtnClicked, setClosedAtBtnClick] = useState(false)
+  const [leadsData, setLeadsData] = useState([])
+  const [salesAgents, setSalesAgents] = useState([])
+
   const [openFilterInput, setOpenFilterInput] = useState("")
+  const [properties, setProperties] = useState({})
+
+  function getAgentNameById(id) {
+    const agent = salesAgents.find((agent) => agent._id === id)
+    return agent.name
+  }
+
+  function capitalizeFirstLetter(string) {
+    const String = string.trim()
+    const array = String.split(" ")
+    const updatedArray = array.map((word) => {
+      const result = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      return result
+    })
+    return updatedArray.join(" ")
+  }
+
+  async function getIdByAgentName(name) {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/agents/name/${name}`,
+      )
+      const arrayOfId = response.data.map((agent) => agent._id)
+      return arrayOfId
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async function handleClick() {
+    const inputField = document.querySelector("#input")
+    const inputValue = inputField.value
+    if (inputValue) {
+      const updatedInputValue = capitalizeFirstLetter(inputValue)
+      const updatedProperties = {
+        ...properties,
+      }
+
+      if (openFilterInput === "salesAgent") {
+        const arrayOfAgentsId = await getIdByAgentName(updatedInputValue)
+        updatedProperties.salesAgent = { $in: arrayOfAgentsId }
+      } else {
+        updatedProperties[openFilterInput] = updatedInputValue
+      }
+
+      const updatedPropertiesString = JSON.stringify(updatedProperties)
+
+      const response = await filterLeadsByProperties(updatedPropertiesString)
+      setLeadsData(response.data)
+      setProperties(updatedProperties)
+    } else {
+      delete properties[openFilterInput]
+
+      const propertiesString = JSON.stringify(properties)
+
+      const response = await filterLeadsByProperties(propertiesString)
+      setLeadsData(response.data)
+      setProperties(properties)
+    }
+  }
+
+  async function getLeadData() {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/leads?minDay=0&maxDay=30",
+      )
+      setLeadsData(response.data)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async function filterLeadsByProperties(filtersString) {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/leads?minDay=0&maxDay=30&filters=${filtersString}`,
+      )
+      return response
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async function getAgentData() {
+    try {
+      const response = await axios.get("http://localhost:3000/agents")
+      setSalesAgents(response.data)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    getLeadData()
+    getAgentData()
+  }, [])
+
+  async function removePropertyFilter(property) {
+    delete properties[property]
+    const propertiesString = JSON.stringify(properties)
+    const response = await filterLeadsByProperties(propertiesString)
+    setLeadsData(response.data)
+    setProperties(properties)
+  }
+
+  async function clearAllFilters() {
+    Object.keys(properties).forEach((key) => delete properties[key])
+    const propertiesString = JSON.stringify(properties)
+    const response = await filterLeadsByProperties(propertiesString)
+    setLeadsData(response.data)
+    setProperties(properties)
+  }
 
   return (
     <div>
@@ -30,12 +145,22 @@ export default function Leads() {
                 <h2 className={`${styles.text1}`}>Leads</h2>
                 <h5 className={`${styles.text2}`}>The Potential Customers</h5>
               </div>
-              <Link
-                to="/addLead"
-                className={`btn btn-outline-success ${styles.add_people_btn}`}
-              >
-                Add New Lead
-              </Link>
+              <div className="d-flex gap-3">
+                {Object.keys(properties).length !== 0 && (
+                  <button
+                    className="btn btn-outline-danger"
+                    onClick={clearAllFilters}
+                  >
+                    Clear All Filters
+                  </button>
+                )}
+                <Link
+                  to="/addLead"
+                  className={`btn btn-outline-success ${styles.add_people_btn}`}
+                >
+                  Add New Lead
+                </Link>
+              </div>
             </div>
             <div className={`${tableStyles.table_wrapper}`}>
               <div className={`${tableStyles.table_container}`}>
@@ -52,6 +177,12 @@ export default function Leads() {
                       className={`form-control ${tableStyles.input}`}
                       type="text"
                     />
+                    <button
+                      className="btn btn-success btn-sm mt-3"
+                      onClick={handleClick}
+                    >
+                      Apply
+                    </button>
                     <i
                       className={`bi bi-x-lg ${tableStyles.close}`}
                       onClick={() => setOpenFilterInput("")}
@@ -125,9 +256,15 @@ export default function Leads() {
                               </div>
                               <div
                                 className={`btn ${tableStyles.button}`}
-                                onClick={() => setOpenFilterInput("Name")}
+                                onClick={() => setOpenFilterInput("name")}
                               >
                                 Filter
+                              </div>
+                              <div
+                                className={`btn text-danger ${tableStyles.button}`}
+                                onClick={() => removePropertyFilter("name")}
+                              >
+                                Remove Filter
                               </div>
                             </div>
                           )}
@@ -164,9 +301,15 @@ export default function Leads() {
                               </div>
                               <div
                                 className={`btn ${tableStyles.button}`}
-                                onClick={() => setOpenFilterInput("Source")}
+                                onClick={() => setOpenFilterInput("source")}
                               >
                                 Filter
+                              </div>
+                              <div
+                                className={`btn text-danger ${tableStyles.button}`}
+                                onClick={() => removePropertyFilter("source")}
+                              >
+                                Remove Filter
                               </div>
                             </div>
                           )}
@@ -205,11 +348,17 @@ export default function Leads() {
                               </div>
                               <div
                                 className={`btn ${tableStyles.button}`}
-                                onClick={() =>
-                                  setOpenFilterInput("Sales Agent")
-                                }
+                                onClick={() => setOpenFilterInput("salesAgent")}
                               >
                                 Filter
+                              </div>
+                              <div
+                                className={`btn text-danger ${tableStyles.button}`}
+                                onClick={() =>
+                                  removePropertyFilter("salesAgent")
+                                }
+                              >
+                                Remove Filter
                               </div>
                             </div>
                           )}
@@ -246,9 +395,15 @@ export default function Leads() {
                               </div>
                               <div
                                 className={`btn ${tableStyles.button}`}
-                                onClick={() => setOpenFilterInput("Status")}
+                                onClick={() => setOpenFilterInput("status")}
                               >
                                 Filter
+                              </div>
+                              <div
+                                className={`btn text-danger ${tableStyles.button}`}
+                                onClick={() => removePropertyFilter("status")}
+                              >
+                                Remove Filter
                               </div>
                             </div>
                           )}
@@ -285,9 +440,15 @@ export default function Leads() {
                               </div>
                               <div
                                 className={`btn ${tableStyles.button}`}
-                                onClick={() => setOpenFilterInput("Tags")}
+                                onClick={() => setOpenFilterInput("tags")}
                               >
                                 Filter
+                              </div>
+                              <div
+                                className={`btn text-danger ${tableStyles.button}`}
+                                onClick={() => removePropertyFilter("tags")}
+                              >
+                                Remove Filter
                               </div>
                             </div>
                           )}
@@ -324,6 +485,18 @@ export default function Leads() {
                               <div className={`btn ${tableStyles.button}`}>
                                 Sort by DESC
                               </div>
+                              <div
+                                className={`btn ${tableStyles.button}`}
+                                onClick={() => setOpenFilterInput("priority")}
+                              >
+                                Filter
+                              </div>
+                              <div
+                                className={`btn text-danger ${tableStyles.button}`}
+                                onClick={() => removePropertyFilter("priority")}
+                              >
+                                Remove Filter
+                              </div>
                             </div>
                           )}
                         </i>
@@ -358,14 +531,6 @@ export default function Leads() {
                               </div>
                               <div className={`btn ${tableStyles.button}`}>
                                 Sort by DESC
-                              </div>
-                              <div
-                                className={`btn ${tableStyles.button}`}
-                                onClick={() =>
-                                  setOpenFilterInput("Time To Close")
-                                }
-                              >
-                                Filter
                               </div>
                             </div>
                           )}
@@ -404,9 +569,15 @@ export default function Leads() {
                               </div>
                               <div
                                 className={`btn ${tableStyles.button}`}
-                                onClick={() => setOpenFilterInput("Closed At")}
+                                onClick={() => setOpenFilterInput("closedAt")}
                               >
                                 Filter
+                              </div>
+                              <div
+                                className={`btn text-danger ${tableStyles.button}`}
+                                onClick={() => removePropertyFilter("closedAt")}
+                              >
+                                Remove Filter
                               </div>
                             </div>
                           )}
@@ -418,35 +589,36 @@ export default function Leads() {
                     </tr>
                   </thead>
                   <tbody>
-                    {leadsData.map((lead) => {
-                      return (
-                        <tr key={lead.id}>
-                          <th scope="row">{lead.id}</th>
-                          <td>{lead.name}</td>
-                          <td>{lead.source}</td>
-                          <td style={{ color: "#70d89d" }}>
-                            {lead.salesAgent}
-                          </td>
-                          <td>{lead.status}</td>
-                          <td>{lead.tags}</td>
-                          <td>{lead.priority}</td>
-                          <td>
-                            {lead.timeToClose
-                              ? `${lead.timeToClose} days`
-                              : "_"}
-                          </td>
-                          <td>{lead.closedAt ? lead.closedAt : "_"}</td>
-                          <td>
-                            <Link
-                              to={`/lead/${lead.id}`}
-                              className="btn btn-success btn-sm"
-                            >
-                              Manage Lead
-                            </Link>
-                          </td>
-                        </tr>
-                      )
-                    })}
+                    {leadsData &&
+                      leadsData.map((lead) => {
+                        return (
+                          <tr key={lead.leadCode}>
+                            <th scope="row">{lead.leadCode}</th>
+                            <td>{lead.name}</td>
+                            <td>{lead.source}</td>
+                            <td style={{ color: "#70d89d" }}>
+                              {getAgentNameById(lead.salesAgent)}
+                            </td>
+                            <td>{lead.status}</td>
+                            <td>{lead.tags}</td>
+                            <td>{lead.priority}</td>
+                            <td>
+                              {lead.timeToClose
+                                ? `${lead.timeToClose} days`
+                                : "_"}
+                            </td>
+                            <td>{lead.closedAt ? lead.closedAt : "_"}</td>
+                            <td>
+                              <Link
+                                to={`/lead/${lead.id}`}
+                                className="btn btn-success btn-sm"
+                              >
+                                Manage Lead
+                              </Link>
+                            </td>
+                          </tr>
+                        )
+                      })}
                   </tbody>
                 </table>
               </div>
